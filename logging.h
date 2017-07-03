a65b8b2f38827d509478e1a949d1c84e41a4a5fe
Raúl Salinas-Monteagudo <raul.salinas@ppro.com>
2017-07-03 09:53:08 +0200

    Add stream-based logger

--- logging.h
+++ logging.h
@@ -1,3 +1,4 @@
+#include <sstream>
 
 std::string nowStr( const char* format = "%F %T" )
 {
@@ -21,3 +22,24 @@ static void logDebug(const std::string &message) {
 void print(const std::string & message, const SDL_AudioSpec obtained) {
     std::clog << message << ": " << obtained.freq << " " << int(obtained.channels) << " chan, " << obtained.samples << " samples" << std::endl;
 }
+
+#define LOG_INFO() LoggerProxy{}
+
+class LoggerProxy  {
+    std::ostringstream oss;
+public:
+    LoggerProxy() {
+    }
+    template<typename T>
+    std::ostream &operator<<(const T & data) {
+        std::clog << "op<<" << std::endl;
+        oss << data;
+        return oss;
+    }
+
+    ~LoggerProxy() {
+        std::clog << "Finished: " << oss.str() << std::endl;
+        oss.str("");
+        oss.clear();
+    }
+};
